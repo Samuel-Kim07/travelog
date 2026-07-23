950de3e36c48732d77c724518df70b30dbd814cf
Samuel-Kim07 <heymanksg@naver.com>
2026-07-24 08:39:21 +0900

    Add files via upload

--- serve.py
+++ serve.py
@@ -2,7 +2,14 @@ import http.server
 import socketserver
 
 PORT = 8000
-Handler = http.server.SimpleHTTPRequestHandler
+class CustomHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
+        self.send_header('Pragma', 'no-cache')
+        self.send_header('Expires', '0')
+        super().end_headers()
+
+Handler = CustomHandler
 
 # Add proper MIME-type mappings to prevent broken images (X-box) on Windows browsers
 Handler.extensions_map.update({
